a03dc884c9ac066439493e369f69112ba475a0d3
Georgi Iliev <georgi.iliev533@outlook.com>
2022-03-29 19:49:55 +0300

    Update worker.py
    Refactoring worker

--- app/worker.py
+++ app/worker.py
@@ -11,31 +11,36 @@ def cleanup_database():
     print("Starting DB cleanup process")
 
     # New db connection is needed to allow parallel execution
-    second_db = mysql.connector.connect(
+    database = mysql.connector.connect(
         host=DB_HOST,
         user=DB_USER,
         password=DB_PASS,
         database=DB_NAME
     )
-    second_db.autocommit = True
-    second_cursor = second_db.cursor(dictionary=True)
+    database.autocommit = True
+    cursor = database.cursor(dictionary=True)
+
     # Find payments that did not go through and the time is up
-    second_cursor.execute("SELECT * FROM payments "
-                          "WHERE status!='succeeded' AND date < DATE_SUB(CURRENT_TIMESTAMP, INTERVAL 10 MINUTE)")
-    payments = second_cursor.fetchall()
+    cursor.execute(
+        "SELECT * FROM payments WHERE status!='succeeded' AND date < DATE_SUB(CURRENT_TIMESTAMP, INTERVAL 10 MINUTE)"
+    )
+    payments = cursor.fetchall()
+
     # Remove buy lock from ideas and allow them to be on the marketplace
     for payment in payments:
-        second_cursor.execute("UPDATE ideas SET buyer_id = NULL WHERE id=%s", (payment["idea_id"],))
         stripe.PaymentIntent.cancel(
             stripe.PaymentIntent(payment["id"])
         )
-        second_cursor.execute("DELETE FROM payments WHERE id = %s", (payment["id"],))
+        cursor.execute("DELETE FROM payments WHERE id = %s", (payment["id"],))
+        cursor.execute("UPDATE ideas SET buyer_id = NULL WHERE id=%s", (payment["idea_id"],))
+
     # Delete old categories and likes from ideas that were deleted
-    second_cursor.execute("DELETE FROM ideas_categories WHERE idea_id NOT IN (SELECT id FROM ideas)")
-    second_cursor.execute("DELETE FROM ideas_likes WHERE idea_id NOT IN (SELECT id FROM ideas)")
+    cursor.execute("DELETE FROM ideas_categories WHERE idea_id NOT IN (SELECT id FROM ideas)")
+    cursor.execute("DELETE FROM ideas_likes WHERE idea_id NOT IN (SELECT id FROM ideas)")
+
     # Close cursor and db everything is complete!
-    second_cursor.close()
-    second_db.close()
+    cursor.close()
+    database.close()
     print("DB cleaning process is completed!")
 
 
