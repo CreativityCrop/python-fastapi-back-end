01f259eaf492234927a85e5eef46bbc28a5cc723
Georgi Iliev <georgi.iliev533@outlook.com>
2022-04-13 21:48:16 +0300

    Worker converted to async mysql driver

--- worker.py
+++ worker.py
@@ -1,4 +1,5 @@
-import mysql.connector
+import asyncmy
+import asyncio
 import stripe
 import requests
 import json
@@ -10,46 +11,39 @@ from app.cache import invalidate_ideas
 stripe.api_key = str(STRIPE_API_KEY)
 
 
-def cleanup_database():
+async def cleanup_database():
     print("Starting DB cleanup process")
 
-    # New db connection is needed to allow parallel execution
-    database = mysql.connector.connect(
-        host=DB_HOST,
-        user=DB_USER,
-        password=DB_PASS,
-        database=DB_NAME
-    )
-    database.autocommit = True
-    cursor = database.cursor(dictionary=True)
+    database = await asyncmy.connect(host=DB_HOST, user=DB_USER, password=DB_PASS, database=DB_NAME, autocommit=True)
+    cursor = database.cursor(cursor=asyncmy.cursors.DictCursor)
 
     # Find payments that did not go through and the time is up
-    cursor.execute(
+    await cursor.execute(
         "SELECT * FROM payments WHERE status!='succeeded' AND date < DATE_SUB(CURRENT_TIMESTAMP, INTERVAL 10 MINUTE)"
     )
-    payments = cursor.fetchall()
+    payments = await cursor.fetchall()
 
     # Remove buy lock from ideas and allow them to be on the marketplace
     for payment in payments:
         stripe.PaymentIntent.cancel(
             stripe.PaymentIntent(payment["id"])
         )
-        cursor.execute("DELETE FROM payments WHERE id = %s", (payment["id"],))
-        cursor.execute("UPDATE ideas SET buyer_id = NULL WHERE id=%s", (payment["idea_id"],))
+        await cursor.execute("DELETE FROM payments WHERE id = %s", (payment["id"],))
+        await cursor.execute("UPDATE ideas SET buyer_id = NULL WHERE id=%s", (payment["idea_id"],))
     if payments is not None:
         invalidate_ideas()
 
     # Delete old categories and likes from ideas that were deleted
-    cursor.execute("DELETE FROM ideas_categories WHERE idea_id NOT IN (SELECT id FROM ideas)")
-    cursor.execute("DELETE FROM ideas_likes WHERE idea_id NOT IN (SELECT id FROM ideas)")
+    await cursor.execute("DELETE FROM ideas_categories WHERE idea_id NOT IN (SELECT id FROM ideas)")
+    await cursor.execute("DELETE FROM ideas_likes WHERE idea_id NOT IN (SELECT id FROM ideas)")
 
     # Delete users that did not verify their accounts after 15 days, there is check if user has ever logged in, if they
     # have and verified is set to 0, then the account is disabled by the administrators
-    cursor.execute(
+    await cursor.execute(
         "SELECT id, first_name, email FROM users "
         "WHERE verified=0 AND date_register < DATE_SUB(CURRENT_TIMESTAMP, INTERVAL 15 DAY) AND date_login IS NULL "
     )
-    users = cursor.fetchall()
+    users = await cursor.fetchall()
     for user in users:
         requests.post(
             "https://api.eu.mailgun.net/v3/app.creativitycrop.tech/messages",
@@ -65,13 +59,13 @@ def cleanup_database():
                 })
             }
         )
-        cursor.execute("DELETE FROM users WHERE id=%s", (user["id"],))
+        await cursor.execute("DELETE FROM users WHERE id=%s", (user["id"],))
 
     # Close cursor and db everything is complete!
-    cursor.close()
+    await cursor.close()
     database.close()
     print("DB cleaning process is completed!")
 
 
 if __name__ == "__main__":
-    cleanup_database()
+    asyncio.run(cleanup_database())
